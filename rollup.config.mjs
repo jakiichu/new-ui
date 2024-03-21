37c0638ade493231b20eb56e6de7d5e382e5504b
zelld <zeLLda444@gmail.com>
2024-03-21 11:44:33 +0700

    add tailwind

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-
+import postcss from 'rollup-plugin-postcss'
+ 
 const packageJson = require("./package.json");
 
 export default [
@@ -21,6 +22,7 @@ export default [
       },
     ],
     plugins: [
+      postcss({extract: true}),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
