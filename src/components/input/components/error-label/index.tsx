01ca69dfc34d10db0e74e7e37cb26074ec381f96
zelld <zeLLda444@gmail.com>
2024-03-29 00:02:27 +0700

    1.1.31

--- src/components/input/components/error-label/index.tsx
+++ src/components/input/components/error-label/index.tsx
@@ -2,7 +2,7 @@ import React, {ReactElement} from 'react';
 import {IInputComponentProps} from "../../interface";
 
 const ErrorLabel = ({error}: Pick<IInputComponentProps, 'error'>): ReactElement => error &&
-    <p className="absolute text-xs leading-6 text-[#E70000]">{error}</p>
+    <p className="absolute text-xs text-[#E70000]">{error}</p>
 
 
 export default ErrorLabel;
